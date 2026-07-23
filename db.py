3b345a6e1eb1175aba1e322e68354ea8b52e07b1
Geo <geometricisomer0@gmail.com>
2026-07-23 21:28:42 +0200

    setup db or something

--- db.py
+++ db.py
@@ -3,3 +3,30 @@ db.py
 
 Handles connection with data base.
 """
+
+import sqlite3
+from main import DB_PATH
+
+conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+conn.execute("PRAGMA foreign_keys = ON")
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
+
+def setup():
+    """
+    Creates tables, if they do not exist already.
+
+    Returns: 
+        None
+    """
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS gifs (
+        uuid TEXT PRIMARY KEY,
+        link TEXT,
+        name TEXT,
+        tags TEXT,
+        added_by INTEGER,
+        added_time INTEGER,
+    )
+    """)
+    cur.execute()
--- main.py
+++ main.py
@@ -12,4 +12,17 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 if not TOKEN: raise ValueError("Discord Bot Token was not set or found.")
+DB_PATH = os.getenv("DB_PATH")
+if not DB_PATH: raise ValueError("Path for data base was not set or found.")
 
+class Bot(commands.Bot):
+    async def setup_hook(self):
+        return await super().setup_hook()
+
+bot = Bot(command_prefix="&&&")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(TOKEN)
